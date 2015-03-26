de7d2c11371f46e2a74d80d6db8b6a53ce8b9084
Joseph Morain <aelphaeis@live.com>
2015-03-26 15:58:42 -0400

    Init

---
+++ src/interfaces/IStatable.java
@@ -0,0 +1,11 @@
+package interfaces;
+
+public interface IStatable {
+	Double getIntelligence();
+	Double getStrength();
+	
+	Double getDexterity();
+	Double getAgility();
+	
+	Double getVitality();
+}
